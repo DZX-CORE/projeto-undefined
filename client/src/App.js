ffb0d1b031fe986a2b2db01f74e04fa81cd591b2
DZX-CORE <dzxcore@gmail.com>
2025-06-11 17:23:07 -0300

    Add client/src/App.js with professional structure

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>projeto-undefined</h1>
+        <p>WebApp React funcionando!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
